c9e73cd595927100021646248dcdbf2846812931
adilmulla330-boop <235547797+MullaAdil@users.noreply.github.com>
2026-08-01 15:50:52 +0530

    fix: restore missing imports in TemplateShowcase

--- src/components/TemplateShowcase.jsx
+++ src/components/TemplateShowcase.jsx
@@ -32,6 +32,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { useResume } from '../context/ResumeContext';
 import { templates, allTags } from './templatesList';
 import TemplateRenderer from './TemplateRenderer';
+import { mockResumeData, templateMockData } from '../utils/mockResumeData';
 import AuraHeader from './AuraHeader';
 import { ZoomIn, Eye, X, Check, Layout, Search } from 'lucide-react';
 
